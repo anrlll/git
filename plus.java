df79c1edc041ce552c19d94f56cf6550efe1389f
{anriii} <{anriii.3939@gmail.com}>
2022-04-20 14:16:55 +0900

    makecode

---
+++ plus.java
@@ -0,0 +1,17 @@
+class Plus{
+    public static void main(String[] args){
+        int plus=0;
+        for(int i=0;i<6;i++){
+            plus += i;
+        }
+        System.out.println(plus);
+        
+        plus = 0;
+        for(int i=0;i<5;i++){
+            plus += i;
+        }
+        System.out.println(plus);
+
+        
+    }
+}
